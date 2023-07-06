f39c926a74b8a92ba23447e899bb61c19db5f974
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-06 17:50:09 +0100

    functions created in js but still need changes

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-function conver(){
-    let temp = document.getElementById("number").value
-    document.getElementById("celData").innerHTML = "temp*5"
+
+var lis = document.getElementById("cel");
+    lis.addEventListener("click",)
+function mouseo(){
+    document.getElementById("mouse").innerHTML = "mouse";
+
+}
+function converC(){
+    var temp = document.getElementById("cel").value;
+    
+    fah = (temp*1.8)+32;
+    kel =  (temp*1) + 273.15;
+   
+    document.getElementById("test").innerHTML = fah + " Fahrenheit";
+    document.getElementById("test1").innerHTML = kel + " Kelvin";
+    console.log(fah + " in Console")
+
+    document.getElementById("fah").setAttribute("value",fah);
+    document.getElementById("kel").setAttribute("value",kel);
+
+}
+function converF(){
+    var temp = document.getElementById("fah").value;
+
+    cel = (temp - 32)*(5/9);
+    kel = (temp - 32)*(5/9)+273.15;
+   
+    document.getElementById("test2").innerHTML = cel + " Celcius";
+    document.getElementById("test3").innerHTML = kel + " Kelvin";
+
+    document.getElementById("cel").setAttribute("value",cel);
+    document.getElementById("kel").setAttribute("value",kel);
+}
+function converK(){
+    var temp = document.getElementById("kel").value
+
+    cel = (temp - 273.15);
+    fah = (temp - 273.15)*(9/5) + 32;
+
+    document.getElementById("test4").innerHTML = cel + " Celcius";
+    document.getElementById("test5").innerHTML = fah + " Fahrenheit";
+
+    document.getElementById("cel").setAttribute("value",temp - 273.15);
+    document.getElementById("fah").setAttribute("placeholder",fah);
 }
